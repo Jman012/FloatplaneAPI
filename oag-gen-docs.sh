55ac10750956ed04c0216c0d1fb45a30c2409d9c
James Linnell <james.n.linnell@gmail.com>
2021-10-17 16:22:39 -0700

    Add initial API specification and supporting files

---
+++ oag-gen-docs.sh
@@ -0,0 +1,1 @@
+rm -rf Docs/*; openapi-generator generate -i floatplaneopenapi_v10.json -o Docs -g html2 --global-property models,supportingFiles,apis=Activation:Auth:CDN:Comment:ConnectedAccounts:Content:Creator:CreatorSubscriptionPlan:Edges:FAQ:Iframe:Livestream:LoyaltyRewards:Playlist:Socket:Subscriptions:Sync:User:WebNotification
