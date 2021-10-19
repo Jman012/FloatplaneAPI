54a4f935d59af79b3950e98cc6f328b24fa3c267
James Linnell <james.n.linnell@gmail.com>
2021-10-19 16:12:34 -0700

    Remove tags in trim operation. Add description to listInvoices.

--- src/trim.js
+++ src/trim.js
@@ -7,11 +7,9 @@ fs.readFile("../floatplane-openapi-specification.json", "utf8", (err, data) => {
 		const spec = JSON.parse(data);
 
 		for (var path in spec.paths) {
-			console.log(path + " " + spec.paths[path]);
 			for (var method in spec.paths[path]) {
-				console.log(path + " " + method);
 				if (spec.paths[path][method].description == "TODO") {
-					console.log(path + " " + method + " <- Removed");
+					console.log("Removed: " + path + " " + method);
 					delete spec.paths[path][method];
 				}
 			}
@@ -21,6 +19,17 @@ fs.readFile("../floatplane-openapi-specification.json", "utf8", (err, data) => {
 			if (Object.keys(spec.paths[path]).length == 0) {
 				delete spec.paths[path];
 			}
+		}
+
+		var tagsToRemove = [];
+		for (var tag in spec.tags) {
+			if (!Object.keys(spec.paths).flatMap(path => Object.keys(spec.paths[path]).map(operation => spec.paths[path][operation])).some(op => op.tags.some(t => t == spec.tags[tag].name))) {
+				console.log("Removing tag " + spec.tags[tag].name);
+				tagsToRemove.push(spec.tags[tag].name);
+			}
+		}
+		for (var tagToRemove of tagsToRemove) {
+			spec.tags = spec.tags.filter(tag => tag.name != tagToRemove);
 		}
 
 		fs.writeFile("../floatplane-openapi-specification-trimmed.json", JSON.stringify(spec, null, 4), "utf8", (err) => {
