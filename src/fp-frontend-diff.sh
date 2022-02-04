ed15d4caf6d8c5aa8d61dd8a47992ce06c4eb353
James Linnell <james.n.linnell@gmail.com>
2022-02-03 23:50:36 -0600

    Update with 3.7.0
    There are no public changes in this. It looks like it has something to do with listing videos as private vs. unlisted vs. public.

--- src/fp-frontend-diff.sh
+++ src/fp-frontend-diff.sh
@@ -6,8 +6,10 @@ then
 	exit 1
 fi
 
-sed -i.bak -e "s/$1/theversion/g" Frontend/$1/app.js
-sed -i.bak -e "s/$2/theversion/g" Frontend/$2/app.js
+before=${1//./\\.}
+after=${2//./\\.}
+sed -i.bak -e "s/$before/theversion/g" Frontend/$1/app.js
+sed -i.bak -e "s/$after/theversion/g" Frontend/$2/app.js
 
 paths=("/app.js" "/vendor.js" "/assets/js/dependencies.js" "/assets/js/video/shaka.mint.js" "/assets/js/video/video.min.js" "/assets/js/video/plugin/chromecast.min.js" "/assets/js/video/plugin/contrib-hls.mint.js" "/assets/js/video/plugin/contrib-shaka.es6.js" "/assets/js/video/plugin/hotkeys.min.js" "/assets/js/video/plugin/resolution-switcher.min.js" "/assets/js/video/plugin/thumbnails.js")
 for path in "${paths[@]}"; do
